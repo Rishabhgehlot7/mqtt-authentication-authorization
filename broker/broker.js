2133705a9236a512572817e836f5ca97d4011bd8
rishabh gehlot <rishabh3x@gmail.com>
2025-09-23 02:18:54 +0530

    minor changes

--- broker/broker.js
+++ broker/broker.js
@@ -9,7 +9,19 @@ const admin = require('firebase-admin');
 const cors = require('cors')
 
 // --- FCM Setup ---
-const serviceAccount = require('./mqtt-broker-47490-firebase-adminsdk-fbsvc-f48d35a38c.json'); // Firebase service account
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
